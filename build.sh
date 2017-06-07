a2889cd5af3b2b864d978398d3829cb452ab81c2
Jakob Goldmann <jakob.goldmannn@radboudumc.nl>
2017-06-07 16:35:54 +0200

    more comments in build instructions

--- build.sh
+++ build.sh
@@ -6,19 +6,17 @@ AUTHOREMAIL="jakob.goldmann@mail.de"
 
 GITURL="https://$GITHUB_PAT@github.com/$TRAVIS_REPO_SLUG.git"
 
-git remote set-url origin $GITURL
-
-git pull
-
-git checkout master
-
 git config --global user.name $AUTHORNAME
-
 git config --global user.email $AUTHOREMAIL
 
+git remote set-url origin $GITURL
+git pull
+git checkout master
 
+
+echo "---- Starting to build Rmd doument ----"
 R CMD BATCH './build.R'
 
-
+echo "---- Deploying build document to Github ----"
 git commit -am "[ci skip] Documents produced in clean environment via Travis $TRAVIS_BUILD_NUMBER"
 git push -u  --quiet origin master
