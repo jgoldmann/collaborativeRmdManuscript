754414718737de5f472b142361a72eb820e02313
Jakob Goldmann <jakob.goldmannn@radboudumc.nl>
2017-06-07 16:21:18 +0200

    addind support to push build manuscript back to repo

--- build.sh
+++ build.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
+AUTHORNAME="Jakbot via Travis-CI"
+
+AUTHOREMAIL="jakob.goldmann@mail.de"
+
+GITURL="https://$GITHUB_PAT@github.com/$TRAVIS_REPO_SLUG.git"
+
+git remote set-url origin $GITURL
+
+git pull
+
+git checkout master
+
+git config --global user.name $AUTHORNAME
+
+git config --global user.email $AUTHOREMAIL
+
+
 R CMD BATCH './build.R'
+
+
+git commit -am "[ci skip] Documents produced in clean environment via Travis $TRAVIS_BUILD_NUMBER"
+git push -u  --quiet origin master
